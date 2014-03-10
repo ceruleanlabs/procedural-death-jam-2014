806326595b61a5b619487dcc57b6158201c178ff
Thomas Jacobs <thmsjcbs@gmail.com>
2014-03-09 23:01:30 -0500

    Add trees to all squares

---
+++ working_title/Assets/Scripts/Editor GUI/Gizmo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gizmo : MonoBehaviour {
+
+	public void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawCube(transform.position, new Vector3(1, 1, 1));
+	}
+}
