749c049c2d3b3c799920ef03ad514531d72b39bf
Steven Pearson <stjpearson@gmail.com>
2014-03-10 21:55:21 -0500

    added random logic. Needs more testing

--- working_title/Assets/Scripts/Game Logic/MazeCreator.cs
+++ working_title/Assets/Scripts/Game Logic/MazeCreator.cs
@@ -7,9 +7,17 @@ public class MazeCreator {
 	public int[] end;
 	private int defaultVal;
 
+	private bool[,] alreadyVisted;
+	private bool[,] solutionPath; // The solution to the maze
+
 	public MazeCreator(int size, int defaultVal) {
 		maze = new int[size, size];
 		this.defaultVal = defaultVal;
+
+		alreadyVisted = new bool[size, size];
+		solutionPath = new bool[size, size];
+
+
 
 		start = new int[2] { (int)(Random.value * size), (int)(Random.value * size) };
 		end = new int[2] { (int)(Random.value * size), (int)(Random.value * size) };
@@ -73,6 +81,8 @@ public class MazeCreator {
 
 			curX += moveX;
 			curY += moveY;
+
+			Debug.Log("X"+curX+"Y"+curY);
 			
 			maze[curX,curY] = 1;
 		}
@@ -89,7 +99,7 @@ public class MazeCreator {
 
 	}
 
-	private bool valid_next(int nextX, int nextY) {
+	private bool validNext(int nextX, int nextY) {
 
 		bool valid = true;
 
@@ -99,6 +109,54 @@ public class MazeCreator {
 		if (nextX > maxX || nextY > maxY || have_visited(nextX, nextY)) valid = false;
 
 		return valid;
+
+		}
+	//Checks if maze is still solvable if we go a direction
+	private bool isSolvable(int x, int y) {
+		int[,] cloned_maze = maze.Clone();
+		int endX = end[0];
+		int endY = end[1];
+		int width = maze.GetLength(0);
+		int height = maze.GetLength(1);
+
+		if (x == endX && y == endY) return true; // If you reached the end
+		if (!validNext(x, y)) return false;  
+		// If you are on a wall or already were here
+		alreadyVisted[x][y] = true;
+		if (x != 0) // Checks if not on left edge
+		if (isSolvable(x-1, y)) { // Recalls method one to the left
+			solutionPath[x][y] = true; // Sets that path value to true;
+			return true;
+		}
+		if (x != width - 1) // Checks if not on right edge
+		if (isSolvable(x+1, y)) { // Recalls method one to the right
+			solutionPath[x][y] = true;
+			return true;
+		}
+		if (y != 0)  // Checks if not on top edge
+		if (isSolvable(x, y-1)) { // Recalls method one up
+			solutionPath[x][y] = true;
+			return true;
+		}
+		if (y != height- 1) // Checks if not on bottom edge
+		if (isSolvable(x, y+1)) { // Recalls method one down
+			solutionPath[x][y] = true;
+			return true;
+		}
+		return false;
+
+
+		}
+
+	// Generate 2 fake mazes
+	private void generateFakes(bool[,] fake1, bool[,] fake2, int size){
+
+			for (int i=0; i<size; i++) {
+				for (int j=0; j<size; j++) {
+					fake1[i][j] = false;
+					fake2[i][j] = false;
+					}
+				}
 
 		}
 
