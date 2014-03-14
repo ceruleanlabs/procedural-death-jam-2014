2f25944cd2f7f22edbf56e0ccf14cba9b21e04e8
Thomas Jacobs <thmsjcbs@gmail.com>
2014-03-13 23:09:23 -0500

    Add respawn option when you beat the game

--- working_title/Assets/Scripts/GUI/GUIController.cs
+++ working_title/Assets/Scripts/GUI/GUIController.cs
@@ -20,8 +20,8 @@ public class GUIController : MonoBehaviour {
 		if(levelDone) {
 			GUI.Box (new Rect (Screen.width/2-50, Screen.height/2-25, 100, 50), "You have escaped the forest!", centeredGui);
 
-			if(GUI.Button(new Rect (Screen.width/2-63, Screen.height/2-25 + 50, 126, 50), "New Adventure +")) {
-				// Reset Level
+			if(GUI.Button(new Rect (Screen.width/2-63, Screen.height/2-25 + 50, 126, 50), "New Adventure (+)")) {
+				levelManager.InitGame();
 			}
 		}
 
