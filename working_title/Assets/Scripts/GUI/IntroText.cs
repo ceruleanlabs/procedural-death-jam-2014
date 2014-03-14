2a6f88a5823686837691c6243881c0bb88713e6e
Thomas Jacobs <thmsjcbs@gmail.com>
2014-03-13 23:23:55 -0500

    Add intro text

---
+++ working_title/Assets/Scripts/GUI/IntroText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroText : MonoBehaviour {
+	private int stage = 0;
+	public GUIStyle centeredGui;
+	
+	void OnGUI () {
+		if(stage == 0) {
+			GUI.Box (new Rect (Screen.width/2-250, Screen.height/2-250, 500, 100), "You are on a quest to get this thing!", centeredGui);
+			
+			if(GUI.Button(new Rect (Screen.width/2-63, Screen.height/2-125 + 50, 126, 50), "Continue")) {
+				stage ++;
+			}
+		}
+		
+		if(stage == 1) {
+			GUI.Box (new Rect (Screen.width/2-250, Screen.height/2-250, 500, 100), "MORE TEXT HERE", centeredGui);
+			
+			if(GUI.Button(new Rect (Screen.width/2-63, Screen.height/2-125 + 50, 126, 50), "Begin Adventure")) {
+				Application.LoadLevel (1); 
+			}
+		}
+	}
+}
